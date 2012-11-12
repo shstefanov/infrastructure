5c9e8f474a5fe4e6c6d60942c97ee2f03fa47c8b
Stefan Stefanov <s.h.stefanov@mail.bg>
2012-11-13 00:21:37 +0200

    wip

--- models.js
+++ models.js
@@ -1,58 +1,42 @@
-
 var helpers = require("./helpers");
-
+var _ = require("underscore");
 
 module.exports = function(mongoose, config, app){
 
   var Schema = mongoose.Schema;
   var models = helpers.loadDirAsObject(config.modelsFolder);
+  //console.log(models);
 
-  var defined = {}; "//{name:prototype}"
-  var createModel = function(name, schema){
-    
-   
-
-    //Check if there is model relation in the schema
-    for (key in schema){
-      var isArray = typeof schema[key] === "array";
-      var value = isArray? schema[key][0] : schema[key];
-      var isString = typeof schema[key] === "string";
-      var isModel = typeof models[value] === "object";
-      var isDefined = typeof defined[schema[key]] === "function";
-
-      if(isArray || isString){//We have a model relation
-        if(isModel){
-          if(isDefined){
-            schema[key] = isArray? [defined[schema[key]]] : defined[schema[key]];
-          }
-          else{
-            //The model is not defined - creating one
-            //value must be string with model name
-            schema[key] = createModel(value, models[value]);
-          }
-        }
-        else{
-          //Hmmm - is string or array, but is not model. What to do?
-          throw new Error("No such model - ", schema[key]);
-        }
-      }
-    }
-
-    //The schema passes trough all above and deserves to be rised to real model
-    //Creating the schema
-    var model = new Schema();
-    model.add(schema);
-
-    //Define the model
-    mongoose.model(name, Model);
-    defined[name] = model;
-
-
-  };
-
-  for(name in models)
-    createModel(name, models[name]);
+  var defined = {}; //{name:prototype}
+  var schemas = {};
   
+  //Declare all models
+  for(key in models){
+    schemas[key] = new Schema();
+    defined[key] = mongoose.model(key, schemas[key]);
+    console.log("registering schema and model:", key);
+  }
+    
+  for(key in models){
+    //building schemas
+    var schema = {};
+    var model = models[key];
+    for(prop in model){
+      var value = _.isArray(model[prop])? model[prop][0] : model[prop];
+      console.log("analizing propertie: ", prop, value);
+      if(typeof value === "string" && models[value]){ //there is such model
+        console.log("found model: ", value);
+        schema[prop] = _.isArray(model[prop])? [schemas[value]] : schemas[value];
+        console.log("propertie: ", prop, "is", schemas[value]);
+      }
+      else{
+        schema[prop] = value;
+      }
+      schemas[key].add(schema);
+    }
+  }
+
+
 
 };
 
