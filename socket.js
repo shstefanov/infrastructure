83b690de0a6733d38cac52216800b6cbf59f2586
Stefan Stefanov <s.h.stefanov@mail.bg>
2013-10-25 03:19:34 +0300

    Removing async.simpleCompose from socket.js

--- socket.js
+++ socket.js
@@ -1,26 +1,6 @@
 var async = require("async");
 var ServiceBuilder = require("./service.js");
 
-// Extending async
-async.simpleCompose = function (fns) {
-  return function () {
-    var that = this;
-    var args = Array.prototype.slice.call(arguments);
-    var callback = args.pop();
-    async.reduce(fns, args, function (newargs, fn, cb) {
-      fn.apply(that, newargs.concat([function () {
-        var err = arguments[0];
-        var nextargs = Array.prototype.slice.call(arguments, 1);
-        cb(err, nextargs);
-      }]))
-    },
-    function (err, results) {
-        callback.apply(that, [err].concat(results));
-    });
-  };
-};
-
-
 
 var colors = require("colors");
 
