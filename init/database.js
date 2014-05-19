99005def61d6c7479658fdb6db3372b7e9f8992e
Stefan Stefanov <s.h.stefanov@mail.bg>
2014-05-19 12:05:44 -0700

    adding user and password to database connection

--- init/database.js
+++ init/database.js
@@ -4,7 +4,11 @@ var mongodb = require("mongodb");
 module.exports = function(cb){
   var config = this.config;
   var MongoClient = mongodb.MongoClient;
-  var dbConnection = "mongodb://"+(config.db.host || "localhost")+":"+(config.db.port || 27017)+"/"+config.db.database
+  var credentials = "";
+  if(config.db.user && config.db.password){
+    credentials = config.db.user +":"+ config.db.password+"@";
+  }
+  var dbConnection = "mongodb://"+credentials+(config.db.host || "localhost")+":"+(config.db.port || 27017)+"/"+config.db.database
   // using native_parser if available:
   // MongoClient.connect('mongodb://127.0.0.1:27017/test'
   //   , {db: {native_parser: true}}, function(err, db) {});
