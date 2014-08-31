16ceb00eebe3c626c7dddf1bd02da989852fa12f
Stefan Stefanov <s.h.stefanov@mail.bg>
2014-09-01 00:50:48 +0300

    improving angularjs support

--- client/angular/app.js
+++ client/angular/app.js
@@ -1,12 +1,53 @@
 io = require("../socket");
 
-require("angular/lib/angular.js");
-angular = ng = window.angular;
-require("ui-router");
+App.EventedClass = require("../../tools/EventedClass");
+App.Controller   = require("../backbone/controller.js");
 
+socket = io.connect();
+
+var initData;
 App.run = function(props){
+  if(!socket.socket.connected){
+    socket = io.connect()
+    .on("connect", function(){
+      setTimeout(function(){
+        socket.emit("init", config.root, function(err, data){
+          if(err) return console.error(err);
+          initData = data;
+          App.run(props);
+        });
+      }, 100);
+    });
+    return;
+  }
+
+  require("angular/lib/angular.js");
+  angular = ng = window.angular;
+  require("ui-router");
+
   app = angular
   .module((props.name || "infrastructureApp"), (props.dependencies||[]).concat(['ui.router']))
+  .factory("socket", function(){
+    return socket;
+  });
+
+  var controllers = {};
+  for(name in initData){ var methods = initData[name];
+    if(methods.error) { console.error(methods.error); continue; }
+    controllers[name] = new App.Controller(name, methods);
+    
+    // Setting up every controller as service with it's own name
+    (function(name, controller){
+      app.factory("controllers."+name, function(){
+        return controller;
+      })
+    })(name, controllers[name])
+  }
+
+  app.factory("controllers", function(){
+    return controllers;
+  });
+
 
   for(var key in props){
     switch(key){
