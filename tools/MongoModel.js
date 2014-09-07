fa45bbbbeb91bb343b66cb347344dc50779c8802
Stefan Stefanov <s.h.stefanov@mail.bg>
2014-09-07 10:03:07 +0300

    improving MongoModel

--- tools/MongoModel.js
+++ tools/MongoModel.js
@@ -1,9 +1,18 @@
 // var AdvancedModel = require("./AdvancedModel");
 // var AdvancedCollection = require("./AdvancedCollection");
 
+
+
 module.exports = function(env){
 
-  var _ = require("underscore");
+  var _       = require("underscore");
+  var mongodb = require("mongodb");
+
+  function createGetRef(name){
+    return function(){
+      return new mongodb.DBRef(name, this.get("_id"));
+    }
+  }
   
   var MongoCollection = env.MongoCollection = env.AdvancedCollection.extend("MongoCollection", {
 
@@ -12,14 +21,65 @@ module.exports = function(env){
   env.MongoModel = env.AdvancedModel.extend("MongoModel", {
     idAttribute: "_id",
     findModel: function(name){return env.Models[name];},
+
+    validate: function(obj){ // all - true/false
+      delete this.error;
+      if(!obj) obj = this.attributes;
+      var error = {}, err = false, props, validation = this.validation;
+      for(var key in validation){
+        if(key==="_id" && this.isNew===true) continue;
+        if(!validation[key](obj[key])){
+          err = true;
+          error[key] = "invalid";
+        }
+      }
+      // Check for unwanted properties
+      for(var key in obj){
+
+        if(!validation[key]){
+          err = true;
+          error[key] = "unwanted";
+        }
+      }
+      if(err){
+        this.error = error;
+        return error;
+      }
+
+    },
+
+
+
+    toJSON: function(){
+      if(this.error) return {error: this.error};
+      return env.AdvancedModel.prototype.toJSON.call(this);
+    },
+
+    constructor: function(data){
+      if(!data){
+        this.error = "Can't find model";
+        return this;
+      }
+      else if(!_.isObject(data)){
+        this.error = "Model should be object";
+      }
+      var err = this.validate(data);
+      if(err){
+        this.error = err;
+        return env.AdvancedModel.apply(this, []);
+      }
+
+      return env.AdvancedModel.apply(this, arguments);
+    }
   },
 
   {
     db: env.db,
 
-    extend: function(name){
+    extend:   function(name){
       var Model = env.AdvancedModel.extend.apply(this, arguments);
       Model.collectionName = name;
+      Model.prototype.getRef = createGetRef(name);
       Model.build = function(cb){
 
         Model.db.createCollection(name, Model.options || {}, function(err, collection){
@@ -33,8 +93,9 @@ module.exports = function(env){
             var ch = [];
             Model.index.forEach(function(i){
               ch.push(function(cb){
-                Model.db.ensureIndex(i.index,i.options||{}, function(){
-                  cb();
+                //TODO - get collection indexes and drop removed if any
+                Model.coll.ensureIndex(i.index,i.options||{}, function(err){
+                  cb(err);
                 }); 
               });
             });
@@ -49,6 +110,92 @@ module.exports = function(env){
       return Model;
     },
 
+
+    // DB methods
+    create:  function(){
+      var Self = this;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      var md = {
+        isNew: true,
+        validation: Self.prototype.validation,
+        attributes: args[0]
+      }
+      var error = Self.prototype.validate.apply(md);
+      if(error) return cb(error);
+      args.push(function(err, doc){ cb(err, new Self(doc)); });
+      Self.coll.insert(md.attributes)
+    },
+
+    find:    function(){
+      var Self = this;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      args.push(function(err, cursor){
+        if(err) return cb(err);
+        cursor.toArray(function(err, docs){
+          if(err) return cb(err);
+          cb(null, docs.map(function(doc){
+            return new Self(doc);
+          }));
+        });
+      });
+      this.coll.find.apply(this, args);
+      return this;
+    },
+
+    findOne: function(){
+      var Self = this;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      args.push(function(err, doc){
+        if(err) return cb(err);
+        var model = new Self(doc);
+        cb(model.error, model);
+      });
+      this.coll.findOne.apply(this, args);
+      return this;
+    },
+
+    remove:  function(){ 
+      var Self = this;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      if(_.isArray(args[0])){
+        env._.amap(args[0], function(model, cb){
+          Self.coll.remove({_id: model.get("_id")}, cb);
+        }, cb);
+      }
+      else{
+        Self.coll.remove({_id: args[0].get("_id")}, cb);
+      }
+      return this;
+    },
+
+    update:  function(){
+      var Self = this;
+      var args = Array.prototype.slice.call(arguments);
+      var cb = args.pop();
+      if(_.isArray(args[0])){
+        env._.amap(args[0], function(model, cb){
+          var err = model.validate();
+          if(err) return cb(err);
+          Self.coll.update(
+            {_id: model.get("_id")}, 
+            model.omit("_id"),
+            cb);
+        }, cb);
+      }
+      else{
+        var err = model.validate();
+        if(err) return cb(err);
+        Self.coll.update({_id: model.get("_id")}, model.omit("_id"), cb);
+      }
+      return this;
+    },
+
+
+    // Models helpers
     queryGenerator: function(modelName, query, options){
       var defaults = Array.prototype.slice.call(arguments);
       defaults.shift();//Remove modelName
@@ -60,7 +207,6 @@ module.exports = function(env){
           defaults.slice(),
           typeof this.contextPattern === "function"? this.contextPattern(modelName, options) : this.contextPattern
         );
-        console.log("running query: ", args);
         Model.coll.find.apply(Model, args);
         return this;
       }
