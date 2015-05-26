7d4084a67262a8263ef2765c70b329dd7e023cf0
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-05-26 04:07:48 +0300

    Fixing helpers and chain builder

--- init/mysql.js
+++ init/mysql.js
@@ -6,6 +6,8 @@ module.exports = function(cb){
 
   if(!config.mysql) return cb();
 
+  var _          = require("underscore");
+  var fs         = require("fs");
   var mysql      = require('mysql');
   var connection = mysql.createConnection(config.mysql);
 
@@ -22,22 +24,65 @@ module.exports = function(cb){
 
   require.extensions['.sql'] = function(module, filename){
     var sql_string = fs.readFileSync(filename, 'utf8').toString();
-    var filters    = [], ctx;
-    var fn = function(data, options, cb){
-      return (ctx || this).query(sql_string, data, cb);
+    var chain    = [], ctx;
+    var exec = function(data, cb){
+      console.log("exec", sql_string, data, cb);
+      var r = this.query(sql_string, data, cb);
+      console.log("exec - after");
+      return r;
     };
-    fn.filter = function(filter_fn){
-      var next = filters.length?filters.pop():fn;
-      var filter = function(){
-        ctx = this;
-        filter_fn.apply(ctx||this, arguments)
-      };
-      filters.push(filter);
-      filter.filter = fn.filter;
-      return filter;
+
+    exec.chain = function(arg){
+      var fn;
+      if(_.isString(arg)){
+        if(arg.indexOf(".")!=-1){
+          fn = function(){
+            var args = Array.prototype.slice.call(arguments);
+            var cb = args.pop();
+            env.do(arg, args, cb);
+          }
+          chain.push(fn);
+        }
+        else{
+          chain.push(function(){
+            var args = Array.prototype.slice.call(arguments);
+            var cb = args.pop();
+            if(!_.isFunction(this[arg])) return cb("Can't find target "+ arg);
+            this[arg].apply(this, arguments);
+          })
+        }
+      }
+      else if(_.isFunction(arg)){
+        chain.push(arg);
+      }
+      else if(_.isObject(arg)){
+        for(var key in arg){
+          (function(prop, val){
+            if(typeof arg[prop] === "string"){
+              arg[prop] = function(){
+                var args = Array.prototype.slice.call(arguments);
+                var cb = args.pop();
+                env.do(val, args, cb);
+              }
+            }
+          })(key, arg[key]);
+        }
+        chain.push(function(){
+          var args = Array.prototype.slice.call(arguments);
+          var cb = args.pop();
+          env.helpers.amap(arg, args, cb);
+        });
+      }
+
+      var composed   = env.helpers.chain(chain.concat([exec]));
+      composed.chain = exec.chain;
+      return composed;
     }
 
-    module.exports = fn;
+
+
+
+    module.exports = exec;
   };
 
   connection.connect(function(err){
