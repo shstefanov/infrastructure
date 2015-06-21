5146281e7d82f6de62adde3ddf0131442f8cf3b1
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-06-21 21:37:43 +0300

    adding sys log reporters for all dataLayers

--- init/loaders/data.js
+++ init/loaders/data.js
@@ -2,6 +2,6 @@ module.exports = function(cb){
   var env    = this;
   env.structureLoader("data", function setupDataLayer(name, DataLayer){
     if(!DataLayer.prototype.name) DataLayer.prototype.name = name;
-    return new DataLayer(env, DataLayer);
+    return new DataLayer(env, DataLayer, name);
   }, cb );
 };
