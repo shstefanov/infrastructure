8188508b640a7cea4cc9d5a6718e2b9857f25f38
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-07-30 21:06:23 +0300

    Change test name for ExtendedModel

--- test/ExtendedModel/validation.js
+++ test/ExtendedModel/validation.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
-var BaseModel = require("../../lib/ExtendedModel.js");
+var ExtendedModel = require("../../lib/ExtendedModel.js");
 
-var Model = BaseModel.extend("Model", {
+var TestModel = ExtendedModel.extend("TestModel", {
   validation: {
     a: function(val){ return val > 5   && val < 10;   },
     b: function(val){ return val > 50  && val < 100;  },
@@ -12,25 +12,25 @@ var Model = BaseModel.extend("Model", {
 describe('Model validation', function() {
   
   it('Empty constructor should give missing error', function (next) {
-    var m = new Model();
+    var m = new TestModel();
     assert.deepEqual(m.error, {a: "missing", b: "missing", c: "missing" });
     next();
   });
 
   it('Should be invalid model', function (next) {
-    var m = new Model({ a:0, b:0, c: 0 });
+    var m = new TestModel({ a:0, b:0, c: 0 });
     assert.deepEqual(m.error, {a: "invalid", b: "invalid", c: "invalid" });
     next();
   });
 
   it('Should be partially invalid model', function (next) {
-    var m = new Model({ a:6, b:60, c: 0 });
+    var m = new TestModel({ a:6, b:60, c: 0 });
     assert.deepEqual(m.error, {c: "invalid" });
     next();
   });
 
   it('Should create redundant error for extra fields', function(next){
-    var m = new Model({ aa:0, bb:0, cc: 0 });
+    var m = new TestModel({ aa:0, bb:0, cc: 0 });
     assert.deepEqual(m.error, {
       aa: "redundant", bb: "redundant", cc: "redundant",
       a:  "missing",   b:  "missing",   c:  "missing"
@@ -39,7 +39,7 @@ describe('Model validation', function() {
   });
 
   it('Should create mixed error' , function(next){
-    var m = new Model({ 
+    var m = new TestModel({ 
       aa: 0, bb: 0, cc: 0, // redundant fields
       a: 0,                // invalid field,
       c: 700               // valid field
