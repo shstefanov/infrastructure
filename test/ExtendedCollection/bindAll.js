28efd877482779a683ccad396177abcf8d5ed632
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-07-31 16:11:59 +0300

    adding ExtendedCollection -> bindAll

---
+++ test/ExtendedCollection/bindAll.js
@@ -0,0 +1,89 @@
+var assert             = require("assert");
+var BaseCollection     = require("../../lib/Collection.js");
+var ExtendedModel      = require("../../lib/ExtendedModel.js");
+var ExtendedCollection = require("../../lib/ExtendedCollection.js");
+
+var TestModel          = ExtendedModel.extend("TestModel", { id_attribute: "id" });
+var TestCollection     = ExtendedCollection.extend("TestCollection", { model: TestModel });
+
+describe('ExtendedCollection -> bindAll', function(){
+
+  it("Using bindAll to listen for model events", function(next){
+    var collection = new TestCollection([
+      { id: 1, field: "a" },
+      { id: 2, field: "a" },
+      { id: 3, field: "b" },
+      { id: 4, field: "b" },
+      { id: 5, field: "b" },
+    ], { group: {field: "field"}} );
+
+    var result, context;
+    collection.bindAll("custom_event", function(data){
+      result  = data;
+      context = this;
+    });
+
+    collection.get(1).trigger("custom_event", "text 1");
+    assert.equal( result, "text 1" );
+    assert.equal( context === collection.get(1), true );
+
+    collection.get(2).trigger("custom_event", "text 2");
+    assert.equal( result, "text 2" );
+    assert.equal( context === collection.get(2), true );
+
+    var removed = collection.get(3);
+    collection.remove(3);
+
+    removed.trigger("custom_event", "text 3");
+    assert.equal( result, "text 2" );
+    assert.equal( context === collection.get(2), true );
+
+    var added = collection.add({id: 6, field: "x"});
+    added.trigger("custom_event", "text 6");
+    assert.equal( result, "text 6" );
+    assert.equal( context === collection.get(6), true );
+
+    next();
+  });
+
+  it("Using unbindAll to stop listening", function(next){
+    var collection = new TestCollection([
+      { id: 1, field: "a" },
+      { id: 2, field: "a" },
+      { id: 3, field: "b" },
+      { id: 4, field: "b" },
+      { id: 5, field: "b" },
+    ], { group: {field: "field"}} );
+
+    var result, context;
+    collection.bindAll("custom_event", function(data){
+      result  = data;
+      context = this;
+    });
+
+    collection.get(1).trigger("custom_event", "text 1");
+    assert.equal( result, "text 1" );
+    assert.equal( context === collection.get(1), true );
+
+    collection.unbindAll("custom_event");
+
+    collection.get(2).trigger("custom_event", "text 2");
+    assert.equal( result, "text 1" );
+    assert.equal( context === collection.get(1), true );
+
+    var removed = collection.get(3);
+    collection.remove(3);
+
+    removed.trigger("custom_event", "text 3");
+    assert.equal( result, "text 1" );
+    assert.equal( context === collection.get(1), true );
+
+    var added = collection.add({id: 6, field: "x"});
+    added.trigger("custom_event", "text 6");
+    assert.equal( result, "text 1" );
+    assert.equal( context === collection.get(1), true );
+
+    next();
+  });
+
+});
