740555ac9d57c50fa43da1d5ab7cca8eed48fc72
Stefan Stefanov <stefan_stefanov@1stonlinesolutions.com>
2015-08-12 15:57:03 +0300

    fixing validation test after fixing issue

--- test/ExtendedModel/validation.js
+++ test/ExtendedModel/validation.js
@@ -14,25 +14,25 @@ var currentFileMark = ["\t\t\t", "[", __filename, "]", "\n"].join("");
 describe('Model -> validation' + currentFileMark, function(){
   
   it('Empty constructor should give missing error', function (next) {
-    var m = new TestModel();
+    var m = new TestModel(undefined, {validate: true});
     assert.deepEqual(m.error, {a: "missing", b: "missing", c: "missing" });
     next();
   });
 
   it('Should be invalid model', function (next) {
-    var m = new TestModel({ a:0, b:0, c: 0 });
+    var m = new TestModel({ a:0, b:0, c: 0 }, { validate: true });
     assert.deepEqual(m.error, {a: "invalid", b: "invalid", c: "invalid" });
     next();
   });
 
   it('Should be partially invalid model', function (next) {
-    var m = new TestModel({ a:6, b:60, c: 0 });
+    var m = new TestModel({ a:6, b:60, c: 0 }, { validate: true });
     assert.deepEqual(m.error, {c: "invalid" });
     next();
   });
 
   it('Should create redundant error for extra fields', function(next){
-    var m = new TestModel({ aa:0, bb:0, cc: 0 });
+    var m = new TestModel({ aa:0, bb:0, cc: 0 }, { validate: true });
     assert.deepEqual(m.error, {
       aa: "redundant", bb: "redundant", cc: "redundant",
       a:  "missing",   b:  "missing",   c:  "missing"
@@ -45,7 +45,7 @@ describe('Model -> validation' + currentFileMark, function(){
       aa: 0, bb: 0, cc: 0, // redundant fields
       a: 0,                // invalid field,
       c: 700               // valid field
-    });
+    }, { validate: true });
     assert.deepEqual(m.error, {
       aa: "redundant", bb: "redundant", cc: "redundant",
       a:  "invalid",   b:  "missing"
