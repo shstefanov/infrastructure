76bd208dcb586699d7c6152c639974603a01dbc7
Stefan Stefanov <stefan_stefanov@1stonlinesolutions.com>
2015-08-12 12:37:57 +0300

    add cleanup i test_env to clean env ang config variables when shut down application

--- test_env.js
+++ test_env.js
@@ -15,26 +15,35 @@ module.exports.start = function(config, cb){
   var config_copy =  JSON.parse(module.exports.stringified_config);
   config_copy.mode = "test";
   infrastructure( config_copy, function(err, env){
-    if(err) return cb(err);
+    if(err) return module.exports.cleanup(err, cb);
     module.exports.env = env;
     cb(null, env);
   });
 }
 
-module.exports.stop = function(cb){
-  if(module.exports.env.config.process_mode === "cluster") cluster.once("disconnect", function(){ cb(); });
-  else module.exports.env.stops.push(function(cb){
-    process.removeAllListeners();
-    cb();
-  });
-  var mode = module.exports.env.config.process_mode;
-  module.exports.env.stop(function(err){
-    if(err) return cb(err);
-    delete module.exports.env;
-    delete module.exports.stringified_config;
-    if(mode === "single") cb(null);
-  });
+module.exports.cleanup = function(err, cb){
+  delete module.exports.env;
+  delete module.exports.stringified_config;
+  cb(err);
 };
+
+module.exports.stop = function(cb){
+
+  var mode = module.exports.env.config.process_mode;
+  
+  if(module.exports.env.config.process_mode === "cluster") cluster.once("disconnect", function(){ 
+    module.exports.cleanup(null, cb);
+  });
+  else module.exports.env.stops.push(function(cb){ process.removeAllListeners(); cb(); });
+
+  module.exports.env.stop(function(err){
+    if(err) return module.exports.cleanup(err, cb);
+    if(mode === "single") module.exports.cleanup(null, cb);
+  });
+
+};
+
+
 
 if(cluster.isWorker){
   module.exports.start(JSON.parse(process.argv[2]), function(err){
