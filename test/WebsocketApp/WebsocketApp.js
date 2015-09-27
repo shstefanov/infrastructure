eeaf75ac68dfb302a91db629c3e63006b740de45
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-09-27 17:35:33 +0300

    Writing WebsocketApp tests

--- test/WebsocketApp/WebsocketApp.js
+++ test/WebsocketApp/WebsocketApp.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 
 var WebsocketApp   = require("../../lib/WebsocketApp.js");
+var helpers        = require("../../lib/helpers.js");
 
 var currentFileMark = ["\t\t\t", "[", __filename, "]", "\n"].join("");
 describe("WebsocketApp" + currentFileMark, function(){
@@ -51,6 +52,7 @@ describe("WebsocketApp" + currentFileMark, function(){
     SessionsCollection: TestSessionsCollection,
 
     env: testEnv,
+
     tokens: {},
 
     path: "/apps/app_name",
@@ -61,15 +63,18 @@ describe("WebsocketApp" + currentFileMark, function(){
     transports: ["websocket", "xhr-polling"],
 
     tokenParam: "token",
+
+    generateAppToken: function(){
+      return "123456"
+    }
   });
 
   it("Instantiates WebsocketApp", function(done){
     testEnv.reset();
     var websocketApp = new TestWebsocketApp(testEnv);
-    assert.deepEqual(testEnv.do_s, [[ 
-      'log.sys',                         
-      'websocket server',                
-      'app_name: [ws://localhost:4001]' ]]);
+    assert.deepEqual(testEnv.do_s, [
+      ['log.sys', 'websocket server', 'app_name: [ws://localhost:4001]' ]
+    ]);
     done();
   });
 
@@ -84,7 +89,7 @@ describe("WebsocketApp" + currentFileMark, function(){
         host: 'localhost',                         
         port: 4001,                                
         name: 'app_name',                          
-        query: 'token=app_name1' 
+        query: 'token=123456'
       });
       var tokenObj = {};
       tokenObj[config.query.split("=").pop()] = "12312313";
@@ -179,6 +184,298 @@ describe("WebsocketApp" + currentFileMark, function(){
         done();
       });
       
+    });
+  });
+
+  it("Builds socket event handlers", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: function(){calls.push(["event_1"].concat(arguments));},
+        event_2: function(){calls.push(["event_2"].concat(arguments));},
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+    assert.equal(Object.keys(websocketApp.__parsedSocketEvents).length, 2);
+    assert.deepEqual(Object.keys(websocketApp.__parsedSocketEvents), [ "event_1", "event_2" ]);
+    done();
+  });
+
+  it("Emits 'init' on bindSocketEvents", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: function(){calls.push(["event_1"].concat(arguments));},
+        event_2: function(){calls.push(["event_2"].concat(arguments));},
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+    assert.deepEqual(test_socket.emits.map(function(args){ return Array.prototype.slice.call(args);}), [
+      [ "init", { "methods": [ "event_1", "event_2" ] } ]
+    ]);
+    done();
+  });
+
+  it("Handle simple websocket events", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: function(){calls.push(["event_1"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+        event_2: function(){calls.push(["event_2"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+        event_3: function(){calls.push(["event_3"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this.handlers[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this.handlers[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+
+    helpers.chain([
+      function(cb){ test_socket.trigger("event_1", {some_data: 11}, function(){ cb(); }); },
+      function(cb){ test_socket.trigger("event_2", {some_data: 12}, function(){ cb(); }); },
+      function(cb){ test_socket.trigger("event_3", {some_data: 13}, function(){ cb(); }); },
+    ])(function(){ 
+
+      var parsed = calls.map(function(call_arr){
+        call_arr[1] = Array.prototype.slice.call(call_arr[1]);
+        delete call_arr[1][3]; // remove the callback
+        return call_arr;
+      })
+
+      assert.deepEqual(parsed, [
+        ["event_1", [ test_socket,  {some_data: 11}, {} ] ],
+        ["event_2", [ test_socket,  {some_data: 12}, {} ] ],
+        ["event_3", [ test_socket,  {some_data: 13}, {} ] ],
+      ]);
+
+      done(); 
+    });
+  });
+
+  it("Handle chained websocket event handler", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: [
+          function(){calls.push(["event_1_1"].concat(arguments)); arguments[2].a = 5; Array.prototype.slice.call(arguments).pop()()},
+          function(){calls.push(["event_1_2"].concat(arguments)); arguments[2].b = 6; Array.prototype.slice.call(arguments).pop()()},
+          function(){calls.push(["event_1_3"].concat(arguments)); arguments[2].c = 7; Array.prototype.slice.call(arguments).pop()()},
+        ]
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this.handlers[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this.handlers[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+
+    helpers.chain([
+      function(cb){ test_socket.trigger("event_1", {some_data: 11}, function(){ cb(); }); },
+    ])(function(){
+
+      var parsed = calls.map(function(call_arr){
+        call_arr[1] = Array.prototype.slice.call(call_arr[1]);
+        delete call_arr[1][3]; // remove the callback
+        return call_arr;
+      })
+
+      assert.deepEqual(parsed, [
+        ["event_1_1", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+        ["event_1_2", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+        ["event_1_3", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+      ]);
+
+      done(); 
+    });
+  });
+
+  it("Handle concurent websocket event handler", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: {
+          "result_a": function(){calls.push(["event_1_1"].concat(arguments)); arguments[2].a = 5; Array.prototype.slice.call(arguments).pop()()},
+          "result_b": function(){calls.push(["event_1_2"].concat(arguments)); arguments[2].b = 6; Array.prototype.slice.call(arguments).pop()()},
+          "result_c": function(){calls.push(["event_1_3"].concat(arguments)); arguments[2].c = 7; Array.prototype.slice.call(arguments).pop()()},
+        }
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this.handlers[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this.handlers[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+
+    helpers.chain([
+      function(cb){ test_socket.trigger("event_1", {some_data: 11}, function(){ cb(); }); },
+    ])(function(){
+      
+      var parsed = calls.map(function(call_arr){
+        call_arr[1] = Array.prototype.slice.call(call_arr[1]);
+        delete call_arr[1][3]; // remove the callback
+        return call_arr;
+      })
+
+      assert.deepEqual(parsed, [
+        ["event_1_1", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+        ["event_1_2", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+        ["event_1_3", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+      ]);
+
+      done(); 
+    });
+  });
+
+  it("Handle combined nested websocket event handler", function(done){
+    testEnv.reset();
+    var calls   = [];
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      listenSocket: {
+        event_1: {
+          "result_a": function(){calls.push(["event_1_1"].concat(arguments)); arguments[2].a = 5; Array.prototype.slice.call(arguments).pop()()},
+          "result_a_1":[
+            function(){calls.push(["event_a_1_1"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+            function(){calls.push(["event_a_1_2"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+            function(){calls.push(["event_a_1_3"].concat(arguments)); Array.prototype.slice.call(arguments).pop()()},
+          ],
+          "result_b": function(){calls.push(["event_1_2"].concat(arguments)); arguments[2].b = 6; Array.prototype.slice.call(arguments).pop()()},
+          "result_c": function(){calls.push(["event_1_3"].concat(arguments)); arguments[2].c = 7; Array.prototype.slice.call(arguments).pop()()},
+        }
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this.handlers[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this.handlers[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+
+    helpers.chain([
+      function(cb){ test_socket.trigger("event_1", {some_data: 11}, function(){ cb(); }); },
+    ])(function(){
+      
+      var parsed = calls.map(function(call_arr){
+        call_arr[1] = Array.prototype.slice.call(call_arr[1]);
+        delete call_arr[1][3]; // remove the callback
+        return call_arr;
+      })
+
+      assert.deepEqual(parsed, [
+        ["event_1_1", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+
+          ["event_a_1_1", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+          ["event_a_1_2", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+          ["event_a_1_3", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+
+        ["event_1_2", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+        ["event_1_3", [ test_socket,  {some_data: 11}, {a: 5, b: 6, c: 7 } ] ],
+      ]);
+
+      done(); 
+    });
+  });
+
+  it("Handle simple 'do_caller' websocket event handler", function(done){
+    testEnv.reset();
+    var TestApp = TestWebsocketApp.extend("TestApp", {
+      env: testEnv,
+      listenSocket: {
+        event_1: "controllers.SomeController.someMethod | socket.a, data.some_data | mount_point"
+      }
+    });
+    var websocketApp = new TestApp(testEnv);
+
+    var test_socket = {
+
+      a: 123,
+
+      emits: [],
+      emit: function(){ this.emits.push(arguments); },
+
+      
+      handlers: {},
+      trigger: function(ev){ this.handlers[ev].apply(this, Array.prototype.slice.call(arguments, 1)); },
+      on:      function(ev, handler){  this.handlers[ev] = handler; }
+
+    };
+
+    var cb_args;
+    websocketApp.bindSocketEvents(test_socket);
+
+    test_socket.trigger("event_1", {some_data: 100}, function(err, result){ 
+      assert.deepEqual(result, { mount_point: 'controllers.SomeController.someMethod' });
+      assert.deepEqual(testEnv.do_s, [ [ 123, 100 ] ]);
+      done();
+    });
+
+    testEnv.do_s.shift(); // remove log report
+
+    testEnv.do_s.forEach(function(do_args){
+      do_args.pop()(null, do_args.shift());
     });
   });
 
