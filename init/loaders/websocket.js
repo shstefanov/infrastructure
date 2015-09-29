b1d40562f88a7a47aef44f85652f6d0ab27fb686
Stefan Stefanov <stefan_stefanov@1stonlinesolutions.com>
2015-09-29 15:28:35 +0300

    moving websocket app settings under 'options' property

--- init/loaders/websocket.js
+++ init/loaders/websocket.js
@@ -2,7 +2,7 @@ module.exports = function(cb){
   var env    = this;
   env.structureLoader("websocket", function setupWebsocket(name, WebsocketApp){
     WebsocketApp.prototype.env  = env;
-    if(!WebsocketApp.prototype.path) Page.prototype.path = "/"+name;
+    if(!WebsocketApp.prototype.options.path) Page.prototype.options.path = "/"+name;
     return new WebsocketApp(env);
   }, cb );
 
