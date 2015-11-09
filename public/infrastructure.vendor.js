65df808efb7317220d2e6eec78c918a89cddb940
Stefan Stefanov <s.h.stefanov@mail.bg>
2015-11-09 22:56:03 +0200

    wip

--- public/infrastructure.vendor.js
+++ public/infrastructure.vendor.js
@@ -145,7 +145,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(6);
-	__webpack_require__(76);
+	__webpack_require__(48);
 	module.exports = __webpack_require__(9);
 
 
@@ -15881,10 +15881,10 @@
 
 /***/ },
 
-/***/ 76:
-[77, 6, 6],
+/***/ 48:
+[64, 6, 6],
 
-/***/ 77:
+/***/ 64:
 /***/ function(module, exports, __webpack_require__, __webpack_module_template_argument_0__, __webpack_module_template_argument_1__) {
 
 	/* WEBPACK VAR INJECTION */(function(global) {/*** IMPORTS FROM imports-loader ***/
