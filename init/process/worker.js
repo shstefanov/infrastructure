aa233015c3e9036d7d79559dfdd633f3097e6577
Stefan Stefanov <s.h.stefanov@mail.bg>
2016-01-04 22:47:53 +0200

    some optimizations

--- init/process/worker.js
+++ init/process/worker.js
@@ -21,58 +21,30 @@ module.exports = function(env, cb){
 
     // Waiting first response. It will provide cached messages
     process.once("message", function(cache){
-      // Change cache caller with the realone
-      env.i.do = DO;
-
-      // Process recieved cache
-      cache.forEach(processMessage);
-
-      // Process locally cached messages
-      local_cache.forEach(function(args){ env.i.do.apply(env.i, args); });
+      env.i.do = DO;//Replace caching caller
+      cache.forEach(processMessage);//Process recieved cache
+      local_cache.forEach(function(args){ env.i.do.apply(env.i, args); });//Process locally cached messages
       delete local_cache;
-
-      // Report that node is initialized to logger
-      env.i.do("log.sys", "worker", _.keys(config.structures).join(","));
-      
-      // Listen for messages
+      env.i.do("log.sys", "worker", _.keys(config.structures).join(","));//Report that node is initialized to logger
       process.on("message", processMessage);
-
-      // Add final stop function - it will remove all intervals or timeouts
-      env.stops.push(function(cb){ process.removeAllListeners(); cb(); });
+      env.stops.push(function(cb){ process.removeAllListeners(); cb(); });//Add final stop function - it will remove all intervals or timeouts
       cb(null, env);
     });
   });
 
   function processMessage(data){
-
     if(data.run_cb) return env.runCallback(data);
-
-    if     (data.cb      ) data.args.push(env.deserializeCallback (data.cb)       );
-    else if(data.listener) data.args.push(env.deserializeListener (data.listener) );
-    else if(data.stream  ) data.args.push(env.deserializeStream   (data.stream)   );
-    
-    var address_parts = data.address.split(".");
-    if(!env.i[address_parts[0]]){
-      console.log(address_parts[0])
-      console.log(env.i);
-      console.error("Can't find target ???");
-    }
-    else{
-      var doArgs = [data.address].concat(data.args);
-      env.i.do.apply(env.i, doArgs);
-    }
+    if(data.cb) data.args.push(env.deserializeCallback(data.cb));
+    else if(data.listener) data.args.push(env.deserializeListener(data.listener));
+    else if(data.stream) data.args.push(env.deserializeStream(data.stream));
+    var doArgs = [data.address].concat(data.args);
+    env.i.do.apply(env.i, doArgs);
   }
   
+  var sl = Array.prototype.slice;
   function deserializeCallback(cb_data){
     cb_data = Array.prototype.slice.call(cb_data);
-    return function(){
-      process.send({
-        run_cb: cb_data,
-        args: Array.prototype.slice.call(arguments),
-        address: cb_data[0]
-      });
-    }
-    // [source, id];
+    return function(){ process.send({ run_cb: cb_data, args: sl.call(arguments), address: cb_data[0] }); }
   }
 
   function forwardToMaster(address, args){
@@ -80,15 +52,8 @@ module.exports = function(env, cb){
     var data = {address: address, args: args};
     if(typeof cb === "function"){
       cb = args.pop();
-      if(cb.name === "do_stream"){
-        data.stream = env.serializeCallback(cb);
-      }
-      else if(cb.name === "do_listener"){
-        data.listener = env.serializeCallback(cb);
-      }
-      else{
-        data.cb = env.serializeCallback(cb);        
-      }
+      if(cb.name === "do_listener") data.listener = env.serializeCallback(cb);
+      else  data.cb = env.serializeCallback(cb);        
     }
     process.send(data);
   }
@@ -102,7 +67,7 @@ module.exports = function(env, cb){
       sl = Array.prototype.slice, 
       resolve = env.helpers.resolve
       i = env.i;
-      
+
 var DO = function(address){
   var args=sl.call(arguments,1)
   var parts=address.split(".")
